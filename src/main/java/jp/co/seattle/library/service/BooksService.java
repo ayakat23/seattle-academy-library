c0677c76767306a9c3eccf3ba8b117a08d4bed82
otomoayaka <otomoayaka@localhost>
2023-04-17 10:39:49 +0900

    task3_一次コミットSQL修正_大友彩佳

--- src/main/java/jp/co/seattle/library/service/BooksService.java
+++ src/main/java/jp/co/seattle/library/service/BooksService.java
@@ -33,7 +33,7 @@ public class BooksService {
 
 		// TODO 書籍名の昇順で書籍情報を取得するようにSQLを修正（タスク３）
 		List<BookInfo> getedBookList = jdbcTemplate.query(
-				"SELECT id, title, author, publisher, publish_date, isbn, description, thumbnail_url FROM books ORDER BY title",
+				"SELECT id, title, author, publisher, publish_date, thumbnail_url FROM books ORDER BY title",
 				new BookInfoRowMapper());
 		return getedBookList;
 	}
