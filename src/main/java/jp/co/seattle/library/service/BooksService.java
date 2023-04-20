d2d905bd3cdb3d52ec53dc1f71ea828560d68dbe
otomoayaka <otomoayaka@192.168.130.18>
2023-04-20 10:47:35 +0900

    task6_謎の改行修正&実行済_大友彩佳

--- src/main/java/jp/co/seattle/library/service/BooksService.java
+++ src/main/java/jp/co/seattle/library/service/BooksService.java
@@ -75,8 +75,7 @@ public class BooksService {
 	 */
 	public void deleteBook(int bookId) {
 		// TODO 対象の書籍を削除するようにSQLを修正（タスク6）
-		String sql = "DELETE FROM books WHERE books.id = ?\n"
-				+ "";
+		String sql = "DELETE FROM books where books.id = ?;";
 		jdbcTemplate.update(sql, bookId);
 	}
 
